9cfa11442596df8108731927ca0b19be3ffe400d
Ethan Nguyen <etnguyen03@hotmail.com>
2018-04-01 18:08:50 -0400

    Add initial code

---
+++ Teamviewer_Hacker/Teamviewer_Hacker/Program.cs
@@ -0,0 +1,61 @@
+﻿// This file is part of Teamviewer_Hacker.
+
+// Teamviewer_Hacker is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Teamviewer_Hacker is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Teamviewer_Hacker.  If not, see<http://www.gnu.org/licenses/>.
+
+// THIS IS AN APRIL FOOLS JOKE. THIS DOES NOT DO ANYTHING HARMFUL.
+// youtube.com/etnguyen03
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teamviewer_Hacker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teamviewer Hacker");
+            Console.WriteLine("Copyright (c) 2018 Ethan Nguyen. All rights reserved");
+            Console.WriteLine("");
+            Console.Write("Enter Teamviewer ID: ");
+            Int64 id = Convert.ToInt64(Console.ReadLine());
+            Console.Clear();
+            Console.Write("For Teamviewer ID ");
+            Console.Write(id);
+            Console.WriteLine(": ");
+            Console.WriteLine("Please select an attack vector");
+            Console.WriteLine("[1] DNS Flood");
+            Console.WriteLine("(2) Denial of Service - UDP Flood");
+            Console.WriteLine("(3) Denial of Service - SYN Flood");
+            Console.WriteLine("(4) Denial of Service - Ping Flood");
+            Console.WriteLine("(5) Degredation of Service");
+            Console.WriteLine("(6) Distributed Denial of Service");
+            Console.WriteLine();
+            Console.Write("Select your attack vector [1]: ");
+            int attack = Convert.ToInt32(Console.ReadLine());
+
+            // Prepare for attacks
+            switch (attack)
+            {
+                case 6:
+
+                default:
+                    break;
+            }
+        }
+    }
+}
